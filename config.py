103d2169e2ee32023442e65cf947c3c2e725ba5f
ivan <ivan.wang.sh@outlook.com>
2021-07-28 19:52:50 -0500

    add demo configure

--- config.py
+++ config.py
@@ -27,14 +27,53 @@ def list_matplotlib_invoker():
     print(f'Available Board Motors: {ks}')
 
 from pprint import pprint
+from collections import OrderedDict
 def demo_config(save=None):
-    config = {}
+    config = OrderedDict()
+    config['version'] = 0.1
+
+    # audio
+    config['audio'] = None
+    config['sr'] = 48000
+
+    # librosa context config
+    strategy = [
+        'rmse_1024_512',
+        'pitchyin_2048_512_0.8'
+    ]
+    config['strategy'] = strategy
+
+    # vibration encoder
+    vibration_enc = [
+        'rmse_enc'
+    ]
+    config['vibration_enc'] = vibration_enc
+
+    # board invoker
+    motors = {
+        # motor, [features,...]
+        'console': ['rmse']
+    }
+    config['motors'] = motors
+
+    pprint(config)
 
     if save is not None:
-        with open(save, 'wb') as f:
-            yaml.dump(config, f)
+        with open(save, 'w') as f:
+            yaml.dump(dict(config), f)
+
+def load_config(cfg):
+    with open(cfg, 'r') as f:
+        cfg = yaml.load(f)
+    
+    ctx = LibrosaContext(audio=cfg['audio'], sr=cfg['sr'], stg=cfg['strategy'])
+    venc = VibrationEncoder(stg=cfg['vibration_enc'])
+    bid = BoardInvoker(motors=cfg['motors'])
+
+    return ctx, venc, bid
 
 if __name__ == '__main__':
     list_librosa_context()
     list_vibration_encoder()
     list_matplotlib_invoker()
+    demo_config('configs/demo.yaml')
