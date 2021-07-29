59230198cc6c7817256c88ca5f3178ac5bc9bbf6
ivan <ivan.wang.sh@outlook.com>
2021-07-28 20:16:54 -0500

    compose mini demo in main

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from matplotlibInvoker import MatplotlibInvoker
 import multiprocessing
 # Producer
 class LibrosaContextProcess(multiprocessing.Process):
-    def __init__(self, io_queue, feat_queue, ctx=None, enc=None):
+    def __init__(self, io_queue, vib_queue, ctx=None, enc=None):
         super().__init__()
         self.io_queue = io_queue
-        self.feat_queue = feat_queue
+        self.vib_queue = vib_queue 
         self.ctx = ctx
         self.enc = enc
     
@@ -23,63 +23,60 @@ class LibrosaContextProcess(multiprocessing.Process):
                 else:
                     self.ctx.sound = chunk
                     features = self.ctx.audio_features()
-                    self.feat_queue.put(features)
-        self.feat_queue.put(None)
+                    vibertion = self.enc.fit(features)
+                    self.vib_queue.put(vibertion)
+        self.vib_queue.put(None)
         return
 
-class FeatureConsumerProcess(multiprocessing.Process):
-    def __init__(self, feat_queue, consumer=None):
+class BroadProcess(multiprocessing.Process):
+    def __init__(self, vib_queue, invoker=None):
         super().__init__()
-        self.feat_queue = feat_queue
-        self.consumer = consumer
+        self.vib_queue = vib_queue
+        self.invoker = invoker
     
     def run(self):
         print('Feature Consumer Process Started..')
+        self.invoker.on_start()
+
         while True:
-            if not self.feat_queue.empty():
-                features = self.feat_queue.get()
-                if features is None:
+            if not self.vib_queue.empty():
+                vib = self.vib_queue.get()
+                if vib is None:
                     print('Exiting Consumer Process...')
                     break
                 else:
-                    print(f'consumer proc feature size {len(features)}')
+                    self.invoker.dispatch(vib)
         return
 
 from utils import load_audio
 from librosaContext import DEFAULT_FRAME_LEN
 from librosaContext import DEFAULT_HOP_LEN
+
+from config import load_config
 def main():
     data, sr = load_audio()
     data = data[:DEFAULT_FRAME_LEN*10]
 
-    ctx = LibrosaContext(sr=48000)
-    ctx.strategy = [
-        'rmse_1024_512',
-        'pitchyin_2048_512_0.8']
-    features = ctx.audio_features()
-    # encoder = VibrationEncoder('')
+    ctx, venc, invoker = load_config('configs/demo.yaml')
 
-    ioQueue = multiprocessing.Queue()
-    featQueue = multiprocessing.Queue()
+    io_queue = multiprocessing.Queue()
+    vib_queue = multiprocessing.Queue()
 
-    librosaProc = LibrosaContextProcess(io_queue=ioQueue, feat_queue=featQueue,
-        ctx=ctx)
-    featProc = FeatureConsumerProcess(feat_queue=featQueue)
+    librosa_proc = LibrosaContextProcess(io_queue=io_queue, vib_queue=vib_queue,
+        ctx=ctx, enc=venc)
+    board_proc = BroadProcess(vib_queue=vib_queue, invoker=invoker)
 
-    librosaProc.start()
-    featProc.start()
+    librosa_proc.start()
+    board_proc.start()
 
     for start in range(0, DEFAULT_FRAME_LEN*9, DEFAULT_FRAME_LEN):
         # print(data[start:start+DEFAULT_FRAME_LEN].shape)
-        ioQueue.put(data[start:start+DEFAULT_FRAME_LEN])
+        io_queue.put(data[start:start+DEFAULT_FRAME_LEN])
 
-    ioQueue.put(None)
+    io_queue.put(None)
 
-    librosaProc.join()
-    featProc.join()
-
-    # mi = MatplotlibInvoker(save_dir='plots')
-    # mi.execture(features)
+    librosa_proc.join()
+    board_proc.join()
 
 # call main function
 main()
