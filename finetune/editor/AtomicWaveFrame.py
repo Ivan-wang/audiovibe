819f5bb979ab06bc19dc01f332aadae0682af27b
ivan <ivan.wang.sh@outlook.com>
2021-12-13 17:14:35 -0600

    atomicwave frame

--- finetune/editor/AtomicWaveFrame.py
+++ finetune/editor/AtomicWaveFrame.py
@@ -1,38 +1,35 @@
 from tkinter import *
 import tkinter.ttk as ttk
 
-import numpy as np 
+import numpy as np
+import pickle
 
-class SliderFrame(Frame):
-    def __init__(self, root=None, **args):
-        # args.setdefault('height', 400)
-        # args.setdefault('width', 800)
-        Frame.__init__(self, root, **args)
+from numpy.core.arrayprint import DatetimeFormat
 
+def load_database():
+    with open('atomic-wave.pkl', 'rb') as f:
+        data = pickle.load(f)
+    
+    return data 
+
+class SliderPanel(LabelFrame):
+    def __init__(self, master=None, **args):
+        LabelFrame.__init__(self, master=master, text='Atomic Wave Sliders', **args)
+
+        self.vars = []
         self.sliders = []
         self.labels = []
-        self.spins = []
-        self.vars = []
-
-        self.sliderPanel = Frame(self, height=400, width=800)
 
         for i in range(24):
-            self.vars.append(DoubleVar())
-            self.sliders.append(Scale(self.sliderPanel, from_=1., to=0., digits=3, resolution=0.01,
+            self.vars.append(DoubleVar(value=0.05))
+            self.sliders.append(Scale(self, from_=1., to=0., digits=3, resolution=0.01,
                 orient=VERTICAL, showvalue=False, sliderlength=30, variable=self.vars[-1]))
-            self.labels.append(Label(self.sliderPanel, textvariable=self.vars[-1]))
+            self.labels.append(Label(self, textvariable=self.vars[-1]))
         
         for i, (s, l) in enumerate(zip(self.sliders, self.labels)):
             # s.grid(row=0, column=i, padx=10, pady=10)
-            s.grid(row=0, column=i)
-            l.grid(row=1, column=i)
-        
-        self.btn = Button(self, text='Save', relief=RAISED)
-
-        self.sliderPanel.pack()
-        self.sliderPanel.pack_propagate(0)
-        ttk.Separator(self, orient=HORIZONTAL).pack(side=BOTTOM, fill=X, expand=YES)
-        self.btn.pack(side=TOP)
+            s.grid(row=0, column=i, padx=1)
+            l.grid(row=1, column=i, padx=1)
 
     def get_values(self):
         return np.array([v.get() for v in self.vars])
@@ -40,36 +37,153 @@ class SliderFrame(Frame):
     def set_values(self, values):
         for v, val in zip(self.vars, values[:24]):
             v.set(val)
+
+class WaveDBFrame(LabelFrame):
+    def __init__(self, master=None, database={}, **args):
+        LabelFrame.__init__(self, master=master, text='Atomic Wave Database', **args)
         
-class DataBaseFrame(Frame):
+        self.database = database
+
+        self.newDBName = StringVar()
+        self.dbNames = sorted(list(self.database.keys()))
+        self.waveNames = []
+
+        self.selFrame = Frame(self)
+        self.dBframe = LabelFrame(self.selFrame, text='Database Name')
+        self.waveFrame = LabelFrame(self.selFrame, text='Wave Name')
+        self.dbOptions = Listbox(self.dBframe, selectmode=SINGLE, exportselection=False)
+        for name in self.dbNames:
+            self.dbOptions.insert(END, name)
+        self.waveOptions = Listbox(self.waveFrame, selectmode=SINGLE, exportselection=False)
+
+        self.btnFrame = Frame(self)
+        self.newDB = Button(self.btnFrame, text='New Database', command=self.__add_wave_database)
+        self.newDBName = Entry(self.btnFrame, textvariable=self.newDBName)
+
+        self.dbOptions.bind('<Double-1>', self.__on_database_selected)
+        # self.waveOptions.bind('<Double-1>', self.__on_waveform_selected)
+
+        self.dbOptions.pack(expand=YES)
+        self.waveOptions.pack(expand=YES)
+
+        self.dBframe.pack(side=LEFT, fill=X, padx=(0, 10))
+        ttk.Separator(self.selFrame, orient=VERTICAL).pack(side=LEFT, fill=Y, expand=YES)
+        self.waveFrame.pack(side=LEFT, fill=X, padx=(10, 0))
+
+        self.newDB.pack(side=LEFT)
+        self.newDBName.pack(side=LEFT)
+
+        self.selFrame.pack(side=TOP, fill=X, expand=YES, padx=5)
+        self.btnFrame.pack(side=TOP, fill=X, expand=YES, padx=5, pady=(5, 0))
+    
+    def set_wave_database_list(self, dbs):
+        self.dbOptions.delete(0, END)
+        for d in dbs:
+            self.dbOptions.insert(END, d)
+
+    def set_waveform_list(self, waves):
+        self.waveOptions.delete(0, END)
+        for w in waves:
+            self.waveOptions.insert(END, w)
+    
+    def get_wave_database_selection(self):
+        sel = self.dbOptions.curselection()
+        return self.dbOptions.get(sel)
+    
+    def get_waveform_selection(self):
+        sel = self.waveOptions.curselection()
+        return self.waveOptions.get(sel)
+   
+    def __on_database_selected(self, event):
+        dbName = self.get_wave_database_selection()
+        waves = sorted(list(self.database[dbName].keys()))
+        self.set_waveform_list(waves)
+    
+    def __add_wave_database(self):
+        name = self.newDBName.get()
+        print(name)
+        if name not in self.database:
+            self.database.setdefault(name, {})
+            self.set_wave_database_list(
+                sorted(list(self.database.keys()))
+            )
+        #TODO: save new name here...
+
+class SliderFrame(Frame):
     def __init__(self, root=None, **args):
+        # args.setdefault('height', 400)
+        # args.setdefault('width', 800)
         Frame.__init__(self, root, **args)
 
-        self.dbVar = StringVar()
-        self.dbNames = ['sample db']
-        self.waveVar = StringVar()
-        self.waveNames = ['a', 'b', 'c', 'd']
+        self.sliderPanel = SliderPanel(self, height=400, width=800)
+       
+        self.sliderPanel.pack()
+        self.sliderPanel.pack_propagate(0) # keep the panel size
+        ttk.Separator(self, orient=HORIZONTAL).pack(side=BOTTOM, fill=X, expand=YES)
+    
+    def set_values(self, values):
+        self.sliderPanel.set_values(values)
+    
+    def get_values(self):
+        return self.sliderPanel.get_values()
 
-        self.dbOptions = OptionMenu(self, 'DataBase', *self.dbNames)
-        self.waveList = Listbox(self, listvariable=self.waveVar, selectmode=SINGLE)
+class WavePlayFrame(LabelFrame):
+    def __init__(self, master=None, **args):
+        LabelFrame.__init__(self, master=master, text='Atomic Wave Playing', **args)
 
-        self.dbOptions.pack(side=BOTTOM)
-        self.waveList.pack(side=BOTTOM)
-        
+        self.wave_len = DoubleVar()
+
+        self.radioFrame = LabelFrame(self, text='Wave Length (in Time)')
+        self.wave_len_options = []
+        for t in [0.5, 1., 2., 5.]:
+            self.wave_len_options.append(
+                Radiobutton(self.radioFrame, text=f'{t} Sec.', variable=self.wave_len,
+                value=t)
+            )
+            self.wave_len_options[-1].pack(anchor=W)
+        self.buttonFrame = Frame(self)
+        self.playButton = Button(self.buttonFrame, text='Play')
+
+        self.playButton.pack(side=RIGHT)
+
+        self.radioFrame.pack(side=TOP, padx=5)
+        self.buttonFrame.pack(side=TOP)
+
+       
 class AtomicWaveFrame(Frame):
     def __init__(self, root=None):
         Frame.__init__(self, root, height=600, width=800)
 
+        self.data = load_database()
+
         self.sliderFrame = SliderFrame(self, height=400)
-        self.sliderFrame.pack()
+        self.controlFrame = Frame(self)
+        self.waveDBFrame = WaveDBFrame(self.controlFrame, database=self.data, height=200)
+        self.playFrame = WavePlayFrame(self.controlFrame)
 
+        self.waveDBFrame.set_wave_database_list(
+            sorted(list(self.data.keys()))
+        )
 
-        x = np.round(np.abs(np.random.randn(24)), 2)
-        self.sliderFrame.set_values(x)
+        self.waveDBFrame.waveOptions.bind('<Double-1>', self.__update_sliders)
+
+        self.waveDBFrame.pack(side=LEFT, padx=10)
+        self.playFrame.pack(side=RIGHT, fill=Y, expand=NO)
+
+        self.sliderFrame.pack(side=TOP, pady=(0, 10), padx=10)
+        self.controlFrame.pack(side=TOP, fill=Y, pady=(0, 10))
+
+    def __update_sliders(self, event):
+        dbName = self.waveDBFrame.get_wave_database_selection()
+        waveName = self.waveDBFrame.get_waveform_selection()
+
+        arr = self.data[dbName][waveName]
+        self.sliderFrame.set_values(arr)
+
 
 
 if __name__ == '__main__':
     root = Tk()
-    f = SliderFrame(root)
+    f = AtomicWaveFrame(root)
     f.pack()
     root.mainloop()
---
+++ finetune/editor/database.py
@@ -0,0 +1,64 @@
+import numpy as np
+import pickle
+
+db = {'uniform': {}, 'linear': {}, 'quadratic': {}}
+
+base0 = np.zeros((24, ))
+base1 = np.ones((24, )) * 0.99
+
+c4_3 = base1.copy()
+c4_3[0::6] = 0.01
+c4_3[1::6] = 0.01
+c4_3[2::6] = 0.01
+
+c4_4 = base1.copy()
+c4_4[0::6] = 0.01
+c4_4[1::6] = 0.01
+
+c4_5 = base1.copy()
+c4_5[0::6] = 0.01
+
+c6_2 = base1.copy()
+c6_2[0::4] = 0.01
+c6_2[1::4] = 0.01
+
+db['uniform'].update({
+    'u2-4': np.array([0.01]*8+[0.99]*4+[0.01]*8+[0.99]*4),
+    'u2-6': np.array([0.01]*6+[0.99]*6+[0.01]*6+[0.99]*6),
+    'u2-8': np.array([0.01]*4+[0.99]*8+[0.01]*4+[0.99]*8),
+    'u4-3': c4_3, 'u4-4': c4_4, 'u4-5': c4_5, 'u6-2': c6_2
+})
+
+l1_12 = np.linspace(0.01, 0.99, num=13, endpoint=True)[1:]
+l1_12 = np.stack([l1_12, np.zeros_like(l1_12)+0.01], axis=-1)
+l1_12 = l1_12.reshape((-1,))
+
+l2_8 = np.linspace(0.01, 0.99, num=7, endpoint=True)[1:]
+l2_8 = np.stack([l2_8, np.zeros_like(l2_8)+0.01], axis=-1)
+l2_8 = np.concatenate([l2_8.reshape((-1,))]*2)
+
+db['linear'].update({
+    'l1_12_a': np.round(l1_12, 2), 'l1_12_d': np.round(l1_12[::-1].copy(), 2),
+    'l2_8_a': np.round(l2_8, 2), 'l2_8_d': np.round(l2_8[::-1].copy(), 2),
+    'l2_8_s': np.round(np.concatenate([l2_8[:12], l2_8[11:-1][::-1]]), 2),
+    'l2_8_si': np.round(np.concatenate([l2_8[11:-1][::-1], l2_8[:12]]), 2),
+})
+
+q1_12 = np.linspace(0., 0.99, num=13, endpoint=True)[1:] ** 2
+q1_12 = np.stack([q1_12, np.zeros_like(q1_12)], axis=-1)
+q1_12 = q1_12.reshape((-1,))
+
+q2_8 = np.linspace(0, 0.99, num=7)[1:] ** 2
+q2_8 = np.stack([q2_8, np.zeros_like(q2_8)+0.01], axis=-1)
+q2_8 = np.concatenate([q2_8.reshape((-1,)), q2_8.reshape((-1,))])
+
+db['quadratic'].update({
+    'q1_12_a': np.round(q1_12, 2), 'q2_12_d': np.round(q1_12[::-1].copy(), 2),
+    'q2_8_a': np.round(q2_8, 2), 'q2_8_d': np.round(l2_8[::-1].copy(), 2),
+    'q2_8_s': np.round(np.concatenate([q2_8[:12], q2_8[11:-1][::-1]]), 2),
+    'q2_8_si': np.round(np.concatenate([q2_8[11:-1][::-1], q2_8[:12]]), 2),
+
+})
+
+with open('atomic-wave.pkl', 'wb') as f:
+    pickle.dump(db, f)
