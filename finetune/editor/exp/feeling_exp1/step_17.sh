42076ac7fc88128a7a018b556a7903d67fce28a2
feit <taofei@feathervibe.com>
2022-02-15 09:17:55 -0800

    change exp1 scripts

--- finetune/editor/exp/feeling_exp1/step_17.sh
+++ finetune/editor/exp/feeling_exp1/step_17.sh
@@ -1,8 +1,15 @@
+sleep_time=3
+
+if [ $# -ne 0 ]
+then
+	sleep_time=$1
+fi
+
 echo "*** signal A ***"
 cd ../.. && python backend.py --scale 100:0 --freq 300 --duty 1 --duration 2.0 --mode periodic_rectangle 1>/dev/null 2>&1 && cd exp/feeling_exp1
 
-# take 7 seconds break
-sleep 7
+echo "take $sleep_time seconds break"
+sleep $sleep_time
 
 echo ""
 echo "*** signal B ***"
