77cebd603a26ea5aae7032c8d143906bf672e0c8
Ivanww <ivan.wang.sh@outlook.com>
2022-03-14 20:17:03 -0500

    add message class

--- vib_music/core/Message.py
+++ vib_music/core/Message.py
@@ -6,16 +6,19 @@ import numpy as np
 @unique
 class MessageT(Enum):
     MSG_RESERVED = auto()
+    # stream control
     MSG_FLAG_ERROR = auto()
     MSG_FLAG_SUCCESS = auto()
-    # control
+    # stream control
     MSG_STREAM_ACQ = auto()
     MSG_STREAM_RESET = auto()
     MSG_STREAM_PULSE = auto()
     MSG_STREAM_RESUME = auto()
+    MSG_STREAM_SEEK = auto()
     MSG_STREAM_STOP = auto()
     MSG_STREAM_DATA = auto()
-    # status
+    # IO control
+    # device status
     MSG_STATUS_ACQ = auto()
     MSG_STATUS_ACK = auto()
 
---
+++ vib_music/core/__init__.py
@@ -0,0 +1,6 @@
+from .FeatureBundle import AudioFeatureBundle
+from .Message import Message, MessageT
+from .StreamDriver import StreamDriverBase
+from .StreamDriver import AudioStreamError, VibrationStreamError
+from .Vibration import Vibration, VibrationFormatError
+from .FeatureBuilder import FeatureBuilder
