0a75b7b2a7e79fbe12ca49b654ab57dd8b95b66d
Ivanww <ivan.wang.sh@outlook.com>
2022-03-18 09:52:53 -0500

    clear up stream event class

--- vib_music/core/StreamEvent.py
+++ vib_music/core/StreamEvent.py
@@ -1,28 +1,18 @@
-from typing import NamedTuple, Optional
-from enum import unique, auto, Enum
-import numpy as np
-
+from typing import Dict, NamedTuple
+from enum import unique, auto, IntEnum
 
 @unique
-class MessageT(Enum):
-    MSG_RESERVED = auto()
-    # stream control
-    MSG_FLAG_ERROR = auto()
-    MSG_FLAG_SUCCESS = auto()
-    # stream control
-    MSG_STREAM_ACQ = auto()
-    MSG_STREAM_RESET = auto()
-    MSG_STREAM_PULSE = auto()
-    MSG_STREAM_RESUME = auto()
-    MSG_STREAM_SEEK = auto()
-    MSG_STREAM_STOP = auto()
-    MSG_STREAM_DATA = auto()
-    # IO control
-    # device status
-    MSG_STATUS_ACQ = auto()
-    MSG_STATUS_ACK = auto()
+class StreamEventType(IntEnum):
+    # events handled by a stream driver
+    STREAM_INIT = auto()
+    STREAM_NEXT_FRAME = auto()
+    STREAM_CLOSE = auto()
+    # event handled by a stream data
+    STREAM_SEEK = auto()
+    # events handled by a stream handler?
+    STREAM_STATUS_ACQ = auto()
+    STREAM_STATUS_ACK = auto()
 
-
-class Message(NamedTuple):
-    header: MessageT
-    what: Optional[np.ndarray] = None
+class StreamEvent(NamedTuple):
+    head: StreamEventType 
+    what: Dict
