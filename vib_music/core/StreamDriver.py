a7a73a8816cd8f155c0d4337467032edf366709f
Ivanww <ivan.wang.sh@outlook.com>
2022-03-18 09:43:21 -0500

    refactor stream driver classes

--- vib_music/core/StreamDriver.py
+++ vib_music/core/StreamDriver.py
@@ -1,63 +1,49 @@
 import abc
-import numpy as np
-from typing import Any, Optional
-from .Message import Message, MessageT
+from typing import Dict, Optional
+
+from .StreamEvent import StreamEventType, StreamEvent
 
 class StreamError(Exception):
-    pass
-
-class AudioStreamError(StreamError):
-    pass
-
-class VibrationStreamError(StreamError):
     pass
 
 class StreamDriverBase(abc.ABC):
     def __init__(self) -> None:
         super(StreamDriverBase, self).__init__()
         self.stream = None
-        self._is_activate = False
 
         # stream handlers
         self.handlers = {
-            MessageT.MSG_STREAM_RESET: self.on_start,
-            MessageT.MSG_STREAM_PULSE: self.on_pulse,
-            MessageT.MSG_STREAM_RESUME: self.on_resume,
-            MessageT.MSG_STREAM_STOP: self.on_close,
-            MessageT.MSG_STREAM_DATA: self.on_receive_frame,
+            StreamEventType.STREAM_INIT: self.on_init,
+            StreamEventType.STREAM_NEXT_FRAME:self.on_next_frame,
+            StreamEventType.STREAM_CLOSE: self.on_close,
 
-            MessageT.MSG_STATUS_ACQ: self.on_status_acq,
+            StreamEventType.STREAM_STATUS_ACQ: self.on_status_acq,
         }
     
     @abc.abstractmethod
-    def on_status_acq(self, what:Any=None) -> Message:
+    def on_status_acq(self, what:Optional[Dict]=None) -> Optional[StreamEvent]:
         raise NotImplementedError()
 
     @abc.abstractmethod
-    def on_start(self, what:Any=None) -> Message:
+    def on_init(self, what:Optional[Dict]=None) -> None:
+        raise NotImplementedError()
+    
+    # @abc.abstractmethod
+    # def on_pulse(self, what:Any=None) -> Message:
+    #     raise NotImplementedError()
+    
+    # @abc.abstractmethod
+    # def on_resume(self, what:Any=None) -> Message:
+    #     raise NotImplementedError()
+    
+    @abc.abstractmethod
+    def on_next_frame(self, what:Optional[Dict]=None) -> None:
         raise NotImplementedError()
     
     @abc.abstractmethod
-    def on_pulse(self, what:Any=None) -> Message:
+    def on_close(self, what:Optional[Dict]=None) -> None:
         raise NotImplementedError()
     
-    @abc.abstractmethod
-    def on_resume(self, what:Any=None) -> Message:
-        raise NotImplementedError()
+    def on_event(self, event:StreamEvent) -> Optional[StreamEvent]:
+        return self.handlers[event.header](event.what)
     
-    @abc.abstractmethod
-    def on_receive_frame(self, what:Optional[np.ndarray]=None) -> Message:
-        raise NotImplementedError()
-    
-    @abc.abstractmethod
-    def on_close(self, what:Any=None) -> Message:
-        raise NotImplementedError()
-    
-    def on_receive_message(self, msg:Message) -> Message:
-        return self.handlers[msg.header](msg.what)
-    
-    @property
-    def is_activate(self) -> bool:
-        # there is no setter for "is_activate"
-        return self._is_activate
-
--- vib_music/drivers.py
+++ vib_music/drivers.py
@@ -1,7 +1,7 @@
-import numpy as np
-from typing import Any, Optional
-from .core import Message, MessageT
-from .core import StreamDriverBase
+from typing import Optional, Dict
+
+from .core import StreamEvent, StreamEventType
+from .core import StreamDriverBase, StreamError
 
 from .env import AUDIO_RUNTIME_READY
 if AUDIO_RUNTIME_READY:
@@ -11,67 +11,41 @@ class AudioDriver(StreamDriverBase):
     def __init__(self) -> None:
         super(AudioDriver, self).__init__()
 
-        self.audio = None
+        self.audio = None # PyAudio object
         self.stream = None
     
-    def on_start(self, what: Any = None) -> Message:
+    def on_init(self, what: Optional[Dict] = None) -> None:
         self.audio = PyAudio()
-        try:
-            self.stream = self.audio.open(
-                format=self.audio.get_format_from_width(what['samplewidth']),
-                channels=what['channels'],
-                rate = what['rate'],
-                output=True
-            )
-        except:
-            return Message(header=MessageT.MSG_FLAG_ERROR)
-        else:
-            self.is_activate = True
-            return Message(header=MessageT.MSG_FLAG_SUCCESS)
+        self.stream = self.audio.open(
+            format=self.audio.get_format_from_width(what['samplewidth']),
+            channels=what['channels'],
+            rate = what['rate'],
+            output=True
+        )
     
-    def on_close(self, what: Any = None) -> Message:
-        try:
-            self.is_activate = False
-            self.stream.stop_stream()
-            self.stream.close()
-            self.audio.terminate()
-        except:
-            return Message(header=MessageT.MSG_FLAG_ERROR)
-        else:
-            return Message(header=MessageT.MSG_FLAG_SUCCESS)
+    def on_close(self, what: Optional[Dict] = None) -> None:
+        self.stream.stop_stream()
+        self.stream.close()
+        self.audio.terminate()
+    
+    def on_pulse(self, what:Optional[Dict]=None) -> None:
+        self.is_activate = False
+        self.stream.stop_stream()
 
-    def on_pulse(self, what: Any = None) -> Message:
-        try:
-            self.is_activate = False
-            self.stream.stop_stream()
-        except:
-            return Message(MessageT.MSG_FLAG_ERROR)
-        else:
-            return Message(MessageT.MSG_FLAG_SUCCESS)
-    
-    def on_resume(self, what: Any = None) -> Message:
-        try:
-            self.is_activate = True
-            self.stream.start_stream()
-        except:
-            return Message(MessageT.MSG_FLAG_ERROR)
-        else:
-            return Message(MessageT.MSG_FLAG_SUCCESS)
-    
-    def on_receive_frame(self, what: Optional[np.ndarray] = None) -> Message:
+    def on_resume(self, what:Optional[Dict]=None) -> None:
+        self.is_activate = True
+        self.stream.start_stream()
+
+    def on_next_frame(self, what: Optional[Dict] = None) -> None:
         if what is not None:
             try:
-                self.stream.write(what)
+                self.stream.write(what['frame'])
             except:
-                return Message(MessageT.MSG_FLAG_ERROR)
-            else:
-                return Message(MessageT.MSG_FLAG_SUCCESS)
-        else:
-            return Message(MessageT.MSG_FLAG_SUCCESS)
+                raise
     
-    # TODO: return audio hardware status
-    def on_status_acq(self, what: Any = None) -> Message:
-        return Message(MessageT.MSG_STATUS_ACK)
+    # TODO: report audio hardware status?
+    def on_status_acq(self, what: Optional[Dict] = None) -> Optional[StreamEvent]:
+        return None
 
 
 from .env import ADC_ENV_READY
@@ -79,45 +53,25 @@ if ADC_ENV_READY:
     import smbus
 
 # NOTE: each sample accept at most 8 operations
-import numpy as np
 class PCF8591Driver(StreamDriverBase):
     def __init__(self) -> None:
         super(PCF8591Driver, self).__init__()
         self.stream = None
 
-    def on_start(self, what:Any=None) -> Message:
+    def on_init(self, what: Optional[Dict] = None) -> None:
         if ADC_ENV_READY:
-            try:
-                self.stream = smbus.SMBus(1)
-            except:
-                return Message(header=MessageT.MSG_FLAG_ERROR)
-            else:
-                self.is_activate = True
-                return Message(header=MessageT.MSG_FLAG_SUCCESS)
+            self.stream = smbus.SMBus(1)
         else:
-            return Message(header=MessageT.MSG_FLAG_ERROR)
+            raise StreamError('Init PCF8591 failed. SMBus not installed.')
 
-    def on_receive_frame(self, what:Optional[np.ndarray]=None) -> Message:
-        for a in what:
-            try:
-                self.stream.write_byte_data(0x48, 0x40, a)
-            except:
-                return Message(header=MessageT.MSG_FLAG_ERROR)
-        return Message(header=MessageT.MSG_STREAM_ACQ)
+    def on_next_frame(self, what: Optional[Dict] = None) -> None:
+        for a in what['frame']:
+            self.stream.write_byte_data(0x48, 0x40, a)
 
-    def on_close(self, what:Any=None) -> Message:
+    def on_close(self, what: Optional[Dict] = None) -> None:
         # close the device?
-        self.is_activate = False
-        return Message(header=MessageT.MSG_FLAG_SUCCESS)
+        return
 
-    def on_pulse(self, what:Any=None) -> Message:
-        self.is_activate = False
-        return Message(header=MessageT.MSG_FLAG_SUCCESS)
-    
-    def on_resume(self, what:Any=None) -> Message:
-        self.is_activate = True
-        return Message(header=MessageT.MSG_FLAG_SUCCESS)
-    
     # TODO: return vibration hardware status
-    def on_status_acq(self, what:Any=None) -> Message:
-        return Message(header=MessageT.MSG_STATUS_ACK)
+    def on_status_acq(self, what: Optional[Dict] = None) -> Optional[StreamEvent]:
+        return StreamEvent(StreamEventType.STREAM_STATUS_ACK, {'status': 'PCF8591'})
