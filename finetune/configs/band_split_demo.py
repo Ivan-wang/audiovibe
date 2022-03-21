7c77255f49985e1cc927ab2a66aa0ac0947e1ca3
feit <taofei@feathervibe.com>
2022-03-21 13:56:59 -0700

    add introduction for split_band_demo

--- finetune/configs/band_split_demo.py
+++ finetune/configs/band_split_demo.py
@@ -3,12 +3,12 @@
 # taofei@feathervibe.com
 
 # vibration
-vib__duty = 0.5
-vib__recep_field = 3
-vib__split_aud = None
-vib__vib_freq = [20,70,120,170,220,270,320,370,420,470]
-vib__vib_scale = [1,1,1,1,1,1,1,1,1,1]
-vib__vib_frame_len = 24
+vib__duty = 0.5    # vibration signal duty ratio, if larger than 1, it represents the number of "1"
+vib__recep_field = 3    # receptive filed in computing spectrogram energy
+vib__split_aud = None    # frequency point for splitting band
+vib__vib_freq = [20,70,120,170,220,270,320,370,420,470]    # output vibration signal's frequency
+vib__vib_scale = [1,1,1,1,1,1,1,1,1,1]    # output vibration signal's scale
+vib__vib_frame_len = 24    # vibration signal sample number for each output frame
 # dsp
-len_window = 512
-len_hop = 256
+len_window = 512    # acoustic feature window length
+len_hop = 256    # acoustic feature hop length
